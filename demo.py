fb0f0b978b5aac67e0cd2822cdbfbbe9efbb9e81
Yuriy Rybak <jura.ryback007@gmail.com>
2025-04-17 23:31:47 +0300

    Reformatted demo code.

--- demo.py
+++ demo.py
@@ -4,25 +4,14 @@ from game_objects import PhysicsBody, Sprite
 
 
 def main():
-    # Creation of the game engine
     engine = GameEngine()
     engine.initialize(800, 600, "My 2D Game")
-
-    # Creation of the scene
     scene = engine.create_scene("MainScene")
-
-    # Creation of the player sprite
     player_sprite = Sprite("Player", "assets/player.png")
     player_sprite.load_texture()
-
-    # Creation of the player physics body
     player_physics = PhysicsBody("PlayerPhysics", 1.0)
     scene.add_object(Leaf("PlayerLeaf", player_sprite))
-
-    # Adding the physics body to the scene
     engine.physics_engine.add_physics_object(player_physics)
-
-    # Running the game engine
     engine.run()
 
 
